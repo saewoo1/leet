0394cfb5d8f6641b495e0f95b2449fe46e9b575d
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2024-04-26 01:23:41 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestIdealString(String s, int k) {
+		int[] dp = new int[26];
+		for (char c : s.toCharArray()) {
+			int maxLen = 0;
+
+			for (int i = Math.max(0, c - 'a' - k); i <= Math.max(25, c - 'a' + k); i++) {
+				maxLen = Math.max(maxLen, dp[i]);
+			}
+			dp[c - 'a'] = maxLen + 1;
+		}
+		int longest = 0;
+		for (int length : dp) {
+			longest = Math.max(longest, length);
+		}
+		return longest;
+    }
+}

946f27482d18c228ee283b0ec7785a48b996a122
saewoo1 <92019454+saewoo1@users.noreply.github.com>
2025-05-05 11:54:29 +0900

    Time: 14 ms (33.06%), Space: 44.6 MB (46.3%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,25 +1,19 @@
 import java.util.*;
 
+/**
+문장을 소문자로, 공백 없이, 알파벳만 남기셈
+ */
 class Solution {
-    // 소문자 알파벳만 남겼을 경우, 앞뒤로 읽었을 때 동일하면 true를 반환하라.
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || Character.isDigit(c)) {
-                sb.append(Character.toLowerCase(c));
-            }
-        }
-        
-        String palindrome = sb.toString();
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-z0-9]", "");
 
-        int start = 0;
-        int end = palindrome.length() - 1;
-        while (start <= end) {
-            if (palindrome.charAt(start) != palindrome.charAt(end)) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left <= right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
                 return false;
             }
-            start++;
-            end--;
         }
         return true;
     }
